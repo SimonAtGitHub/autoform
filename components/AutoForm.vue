b5d8db0a81d707d99a6b544794dd38fc5399e8ff
liuqingqing05 <liuqingqing05@meituan.com>
2017-11-02 12:30:32 +0800

    update

---
+++ components/AutoForm.vue
@@ -0,0 +1,59 @@
+
+<template>
+    <div>
+        <el-form ref="form" :model="model" :label-position="layout.align||'left'" :label-width="layout.labelWidth" :inline="layout.inline">
+            <field v-if="!layout.custom" :ref="'form_'+field.key" v-for="field in fields" :model.sync="model" :field="field" :key="'form_'+field.key" :span.sync="layout.span"></field>
+            <slot :keys="keys"></slot>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    /*eslint-disable */
+    methods: {
+        validate() {
+            return new Promise((resolve, reject) => {
+                this.$refs.form.validate(valid => {
+                    if (valid) {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                })
+            });
+        },
+        resetForm() {
+            this.$refs.form.resetFields();
+        },
+        getComponents() {
+            
+           let result= Object
+                .keys(this.$refs)
+                .reduce((res,item)=>{
+                    res[item]=this.$refs[item][0];
+                    return res;
+                },{});
+
+
+                return  result;
+        }
+    },
+    props: ['model', 'fields', 'layout'],
+    computed: {
+        keys() {
+            let keys = {};
+            this.fields.forEach(field => {
+                keys[field.key] = field;
+            });
+            return keys;
+        }
+    },
+    created() {
+        //make sure that the 'value' is always set
+        this.fields.forEach(field => {
+            if (typeof this.model[field.key] === 'undefined') this.$set(this.model, field.key, '');
+        });
+    }
+};
+</script>
---
+++ components/Field.vue
@@ -0,0 +1,59 @@
+<template>
+    <component v-show="display" :is="type" :field="field" :model="model" :to="field.templateOptions" :span="span">
+    </component>
+</template>
+
+<script>
+    /*eslint-disable */
+    import Util, { getTypes, setError, parseValidationString } from '../util';
+    export default {
+        props: ['form', 'model', 'field', 'to',"span"],
+        computed: {
+            type: function() {
+                return 'form_' + this.field.type;
+            },
+            display: function() {
+                // always show if there is no conditional display
+                let displayType = typeof this.field.display;
+                if (displayType !== 'function' && displayType !== 'string') return true;
+
+                if (displayType === 'function') {
+                    return this.field.display(this.field, this.model);
+                } else {
+                    let result = new Function('field', 'model', 'return ' + this.field.display + ';');
+                    return result.call({}, this.field, this.model);
+                }
+            }
+        },
+        methods: {
+           
+        },
+        components: getTypes(),
+        created() {
+            // this.validate();
+            // this.$watch(function() {
+            //     return this.model[this.field.key];
+            // }, (val) => {
+            //     this.validate();
+            // });
+        },
+        mounted() {
+            if (!this.field.wrapper) return;
+
+            //create a temporary element
+            let wrapper = document.createElement('DIV');
+            //populate it with the wrapper string
+            wrapper.innerHTML = this.field.wrapper;
+            //get the parent
+            let parent = this.$el.parentNode;
+            //insert the wrapper before this element
+            parent.insertBefore(wrapper, this.$el);
+            //append the element to the new wrapper
+            wrapper.firstChild.appendChild(this.$el);
+            //move the new wrapper back to where it should be
+            parent.insertBefore(wrapper.firstChild, wrapper);
+            //remove the temporary element
+            parent.removeChild(wrapper);
+        }
+    };
+</script>
