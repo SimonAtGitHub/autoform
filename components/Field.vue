a0213844d41f8d9e69e92c3316e6a71a6d6ccda6
maominfang <maominfang@meituan.com>
2017-11-13 19:18:40 +0800

    display

--- components/Field.vue
+++ components/Field.vue
@@ -1,7 +1,7 @@
 <template>
-    <el-col :span=" field.templateOptions.span||span">
+    <el-col :span=" field.templateOptions.span||span" v-show="display">
         <el-form-item :label="field.templateOptions.label" :prop="field.key" :rules="field.validators">
-            <component v-show="display" :is="type" :field="field" :model="model" :to="field.templateOptions" :span="span">
+            <component :is="type" :field="field" :model="model" :to="field.templateOptions" :span="span">
             </component>
         </el-form-item>
     </el-col>
