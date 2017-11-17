7c7921547904e08b3f48304a5251a991c72ef540
jinweiyi02 <jinweiyi02@meituan.com>
2017-11-17 16:37:26 +0800

    save

--- components/AutoForm.vue
+++ components/AutoForm.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="autoform-block">
         <el-form ref="form" :model="model" :label-position="layout.align||'left'" :label-width="layout.labelWidth" :inline="layout.inline">
-            <el-row v-if="!layout.inline">
+            <el-row v-if="!layout.inline" :gutter="layout.gutter">
                 <field v-if="!layout.custom" :ref="'form_'+field.key" :event-bus="eventBus"  v-for="field in fields" :model.sync="model" :field="field" :key="'form_'+field.key" :span.sync="layout.span" :inline="layout.inline"></field>
             </el-row>
             <field v-if="!layout.custom && layout.inline" :ref="'form_'+field.key" v-for="field in fields" :model.sync="model" :field="field" :key="'form_'+field.key" :span.sync="layout.span" :inline="layout.inline"></field>
@@ -19,6 +19,7 @@
 import Vue from "vue";
 
 export default {
+  /*eslint-disable */
   methods: {
     validate() {
       return new Promise((resolve, reject) => {
