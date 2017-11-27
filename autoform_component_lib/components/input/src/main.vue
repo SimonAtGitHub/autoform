f18291936c4e2a3045a520192768469824b09ada
jinweiyi02 <jinweiyi02@meituan.com>
2017-11-27 17:54:07 +0800

    fix

--- autoform_component_lib/components/input/src/main.vue
+++ autoform_component_lib/components/input/src/main.vue
@@ -1,54 +1,5 @@
-
-
-<script>
-import basicComponent from "autoform/utils/baseField";
-
-export default {
-  mixins: [basicComponent],
-  name: "cInput",
-  icon: "el-icon-edit",
-  methods: {},
-  render(h) {
-    let props = {
-      field: this.field,
-      to: this.field.templateOptions,
-      span: this.span,
-      eventBus: this.eventBus,
-      model: this.model,
-      form: this.form
-    };
-
-    let vNode = h("el-input", {
-    //   domProps: { "v-model": this.model[this.field.key] },
-      attrs: {
-        value: this.model[this.field.key]
-      },
-      props
-    });
-    return <span>{vNode}</span>;
-
-    //   let vNode = h(this.type, {
-    //     props
-    //   });
-    //   return (
-    //     <el-col span={this.field.templateOptions.span || this.span} offset={this.field.templateOptions.offset}>
-    //       <el-form-item
-    //         label={this.field.templateOptions.label}
-    //         prop={this.field.key}
-    //         rules={this.field.validators}
-    //       >
-    //         {vNode}
-    //       </el-form-item>
-    //     </el-col>
-    //   );
-  }
-};
-</script>
-
-
-
-
-    <!-- <el-input v-model="model[field.key]"
+<template>
+    <el-input v-model="model[field.key]"
         :id="to.id ? to.id : null"
         :placeholder="to.placeholder"
         type="text"
@@ -66,4 +17,16 @@ export default {
         @keydown="onKeydown"
         v-form-atts="to.atts"
         v-form-input-type="to.inputType">
-    </el-input> -->
+    </el-input>
+</template>
+
+<script>
+import basicComponent from "autoform/utils/baseField";
+
+export default {
+  mixins: [basicComponent],
+  name: "cInput",
+  icon: "el-icon-edit",
+  methods: {}
+};
+</script>
