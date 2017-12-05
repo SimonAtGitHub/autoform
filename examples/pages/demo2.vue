a7b0195c9bba247971870513da1343980e4ac533
liuqingqing05 <liuqingqing05@meituan.com>
2017-12-05 13:11:26 +0800

    update  bug

--- examples/pages/demo2.vue
+++ examples/pages/demo2.vue
@@ -21,32 +21,66 @@
                     gutter: 60
                 },
                 fields5: [
-                    {
-                        key: 'name',
-                        type: 'input',
-                        templateOptions: {
-                            label: '审批人',
-                            offset: 2
-                        }
-                    },
-                    {
-                        key: 'status',
-                        type: 'radio',
-                        templateOptions: {
-                            label: '活动状态',
-                            options: [
-                                {
-                                    label: '线上品牌商赞助',
-                                    value: 1
-                                },
-                                {
-                                    label: '线下场地免费',
-                                    value: 0
-                                }
-                            ],
-                            offset: 1
-                        }
-                    }
+                    // {
+                    //     key: 'name',
+                    //     type: 'input',
+                    //     templateOptions: {
+                    //         label: '审批人',
+                    //         offset: 2
+                    //     }
+                    // },
+                    // {
+                    //     key: 'status',
+                    //     type: 'radio',
+                    //     templateOptions: {
+                    //         label: '活动状态',
+                    //         options: [
+                    //             {
+                    //                 label: '线上品牌商赞助',
+                    //                 value: 1
+                    //             },
+                    //             {
+                    //                 label: '线下场地免费',
+                    //                 value: 0
+                    //             }
+                    //         ],
+                    //         offset: 1
+                    //     }
+                    // }
+                     {
+                       key: 'name',
+                       type: 'input',
+                       templateOptions: {
+                           label: '审批人',
+                           offset: 2
+                       }
+                   },
+                   {
+                       key: 'status',
+                       type: 'radio',
+                       templateOptions: {
+                           label: '活动状态',
+                           options: [
+                               {
+                                   label: '线上品牌商赞助',
+                                   value: 1
+                               },
+                               {
+                                   label: '线下场地免费',
+                                   value: 0
+                               }
+                           ],
+                           offset: 1
+                       }
+                   },
+                   {
+                       key: 'name',
+                       type: 'hello',
+                       templateOptions: {
+                           label: '审批人',
+                           offset: 2
+                       }
+                   },
                 ]
             };
         }
